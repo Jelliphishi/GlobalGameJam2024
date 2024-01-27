16480dfc41d204bdb8299a4187d8a74e7e30b54a
jbacarra <143844174+jbacarra@users.noreply.github.com>
2024-01-27 00:10:45 -0500

    Card Implementation stuff not done angry at me
    my googoogaga meter is going babies

---
+++ Assets/Card.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class Card : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    /*
+    string cardName;
+    string cardSuit;
+    string cardType;
+    string cardAbility;
+
+    Card(string name; string suit; string type; string ability){
+        cardName = name;
+        cardSuit = suit;
+        cardType = type;
+        cardAbility = ability;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private bool mouse_over = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mouse_over)
+        {
+            Debug.Log("Mouse Over");
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        mouse_over = true;
+        Debug.Log("Mouse enter");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        mouse_over = false;
+        Debug.Log("Mouse exit");
+    }
+    */
+}
