58a2b4c46ecbdd185a8de312287c4ce321d11393
jbacarra <143844174+jbacarra@users.noreply.github.com>
2024-01-27 19:29:30 -0500

    cards again
    ooooooooooooowahhawa

---
+++ Assets/__Scripts/CardInfo.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardInfo : ScriptableObject
+{
+    public string cardName;
+    public string cardSuit;
+    public string cardType;
+    public string cardAbility;
+}
